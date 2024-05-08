8777e77e71a4439b772fd3f024fd5dc23f1fb7a0
Lluis Simon Argemi <lluis.simon@waynabox.com>
2024-05-08 03:05:55 +0200

    added empty state

--- src/app/Modulos/2-control-flow/module2/module2.component.ts
+++ src/app/Modulos/2-control-flow/module2/module2.component.ts
@@ -1,15 +1,16 @@
-
+import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'module2',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgFor, NgSwitch, NgSwitchCase, NgSwitchDefault],
   templateUrl: './module2.component.html',
   styleUrls: ['./module2.component.scss']
 })
 export class Module2Component {
   variable = true;
   displayColor = true;
+  items: number[] = [1, 2, 3, 4, 5];
   switchValue = 'A';
 }
