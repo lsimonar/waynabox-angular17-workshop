bf951dec94d0fd86607a10fceaa2804baa2bdb1b
Lluis Simon Argemi <lluis.simon@waynabox.com>
2024-05-08 11:27:45 +0200

    solved module 2

--- src/app/Modulos/2-control-flow/module2/module2.component.ts
+++ src/app/Modulos/2-control-flow/module2/module2.component.ts
@@ -1,10 +1,10 @@
-import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'module2',
   standalone: true,
-  imports: [NgIf, NgFor, NgSwitch, NgSwitchCase, NgSwitchDefault],
+  imports: [],
   templateUrl: './module2.component.html',
   styleUrls: ['./module2.component.scss']
 })
