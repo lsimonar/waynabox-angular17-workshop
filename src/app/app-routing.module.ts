754ab78f806d7a995279ffc902cbe8bc069722c2
Lluis Simon Argemi <lluis.simon@waynabox.com>
2024-05-08 12:00:38 +0200

    s

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NonStandaloneComponent } from './Modulos/1-standalone-components/ejercicios/exercice-1/non-standalone-component.component';
 import { IndexComponent } from './index/index.component';
-import { Module2Component } from './Modulos/2-control-flow/module2/module2.component';
-import { Module3Component } from './Modulos/3-lazy-loading/module3/module3.component';
-import { Module4Component } from './Modulos/4-signals/module4/module4.component';
 
 const routes: Routes = [
   {
