315d26eadab6959215deec428f5eb3f764bd4265
Lluis Simon Argemi <lluis.simon@waynabox.com>
2024-05-08 03:30:30 +0200

    creado ejercicio 1

---
+++ src/app/Modulos/4-signals/components/exercise1/exercise1.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'exercise1',
+  standalone: true,
+  imports: [],
+  templateUrl: './exercise1.component.html',
+  styleUrl: './exercise1.component.scss'
+})
+export class Exercise1Component implements OnInit, OnChanges {
+    @Input() counter: number = 0;
+    doubleCounter: number = 0;
+
+    ngOnInit() {
+        this.doubleCounter = this.counter * 2;
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+      this.doubleCounter = this.counter * 2;
+    }
+}
--- src/app/Modulos/4-signals/module4/module4.component.ts
+++ src/app/Modulos/4-signals/module4/module4.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Exercise1Component } from '../components/exercise1/exercise1.component';
 
 @Component({
   selector: 'app-module4',
   standalone: true,
-  imports: [],
+  imports: [Exercise1Component],
   templateUrl: './module4.component.html',
   styleUrl: './module4.component.scss'
 })
 export class Module4Component {
+  counter: number = 0;
 
+  increment() {
+    this.counter++;
+  }
 }
